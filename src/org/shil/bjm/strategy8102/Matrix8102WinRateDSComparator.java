e49eed6435c83f653a73b0141c4f4ba5c7f89c64
vanis <vanis@OKOK>
2018-09-05 16:40:20 +0800

    adjust

--- src/org/shil/bjm/strategy8102/Matrix8102WinRateDSComparator.java
+++ src/org/shil/bjm/strategy8102/Matrix8102WinRateDSComparator.java
@@ -7,9 +7,9 @@ public class Matrix8102WinRateDSComparator implements Comparator<StrategyMatrix8
 	@Override
 	public int compare(StrategyMatrix8012 arg0, StrategyMatrix8012 arg1) {
 		if(arg0.getWdlRateWithDS()[0] > arg1.getWdlRateWithDS()[0]) {
-			return -1;
-		}else if(arg0.getWdlRateWithDS()[0] < arg1.getWdlRateWithDS()[0]) {
 			return 1;
+		}else if(arg0.getWdlRateWithDS()[0] < arg1.getWdlRateWithDS()[0]) {
+			return -1;
 		}
 		return 0;
 	}
